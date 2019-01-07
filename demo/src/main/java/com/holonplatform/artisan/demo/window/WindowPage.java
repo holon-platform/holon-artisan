311c6b09fd4a0fc2f6bf4fbf50339e8ad20d0d19
Fabio Paroni <fparoni@gmail.com>
2019-01-07 09:58:02 +0100

    Fix

--- demo/src/main/java/com/holonplatform/artisan/demo/window/WindowPage.java
+++ demo/src/main/java/com/holonplatform/artisan/demo/window/WindowPage.java
@@ -42,7 +42,7 @@ public class WindowPage extends SplitLayout {
 
 	private final Label codeLabel;
 
-	private final static String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
+	private static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
 			+ "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
 			+ "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
 			+ "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
@@ -55,7 +55,7 @@ public class WindowPage extends SplitLayout {
 			+ "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
 			+ "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est "
 			+ "laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-			+ " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ";
+			+ " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor...";
 
 	public WindowPage() {
 		super();
