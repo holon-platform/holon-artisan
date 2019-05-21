55f23bf9266d11e96d937d846da861cd2f23fbf8
Riccardo Righi <rr9379@gmail.com>
2019-05-21 15:07:46 +0200

    remove app parent

--- demo/src/main/java/com/holonplatform/artisan/demo/Application.java
+++ demo/src/main/java/com/holonplatform/artisan/demo/Application.java
@@ -17,18 +17,11 @@ package com.holonplatform.artisan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ServletComponentScan
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
